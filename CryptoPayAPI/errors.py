1f40da0c4a221bb90203ed6cd43af0f82def0df0
Maxim Mosin <max@mosin.pw>
2022-12-27 11:49:56 +0900

    Use Optional in errors.BaseError

--- CryptoPayAPI/errors.py
+++ CryptoPayAPI/errors.py
@@ -3,11 +3,14 @@
 #  CryptoPayAPI - API errors.
 #  Created by LulzLoL231 at 2/6/22
 #
+from typing import Optional
+
+
 class BaseError(Exception):
     def __init__(self,
                  *args: object,
-                 raw_response: dict | None = None,
-                 raw_headers: dict | None = None) -> None:
+                 raw_response: Optional[dict] = None,
+                 raw_headers: Optional[dict] = None) -> None:
         super().__init__(*args)
         self.raw_response = raw_response
         self.raw_headers = raw_headers
