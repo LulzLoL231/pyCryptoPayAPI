24a351e66d19945729b2333daf37f28556bc6f16
Maxim Mosin <max@mosin.pw>
2023-01-11 18:10:04 +0900

    Add errors docstrings

--- src/CryptoPayAPI/errors.py
+++ src/CryptoPayAPI/errors.py
@@ -7,6 +7,12 @@ from typing import Optional
 
 
 class BaseError(Exception):
+    '''Base error class
+
+    Attributes:
+        raw_response (dict, optional): RAW response from CryptoPay API.
+        raw_headers (dict, optional): RAW headers from CryptoPay API response.
+    '''
     def __init__(self,
                  *args: object,
                  raw_response: Optional[dict] = None,
@@ -17,24 +23,36 @@ class BaseError(Exception):
 
 
 class UnauthorizedError(BaseError):
+    '''Authorization on CryptoPay API is failed, maybe incorect/expired API token.
+    '''
     pass
 
 
 class MethodNotFoundError(BaseError):
+    '''Requested method is not found.
+    '''
     pass
 
 
 class UnexpectedError(BaseError):
+    '''Unexpected API error.
+    '''
     pass
 
 
 class ExpiresInInvalidError(BaseError):
+    '''Value of `expires_in` is invalid.
+    '''
     pass
 
 
 class UpdateSignatureError(BaseError):
+    '''Webhook signature validation error.
+    '''
     pass
 
 
 class MethodDisabledError(BaseError):
+    '''Requested method is disabled. Check bot settings.
+    '''
     pass
